c0d02873fb7bb6278cda8aaedf0965703e94fa3c
Souleimane Kouygou <soulei.dev@gmail.com>
2022-02-14 20:45:56 +0100

    🔨 Refactor ViewImageScreen

--- App.tsx
+++ App.tsx
@@ -2,30 +2,16 @@ import WelcomeScreen from "./src/screens/WelcomeScreen/WelcomeScreen";
 import CustomCard from "./src/components/CustomCard/CustomCard";
 import { SafeAreaView, StyleSheet, View } from "react-native";
 import ListingDetailsScreen from "./src/screens/ListingDetailsScreen/ListingDetailsScreen";
+import ViewImageScreen from "./src/screens/ViewImageScreen/ViewImageScreen";
 
 const App = (): JSX.Element => {
-    return (
-        // <SafeAreaView style={styles.container}>
-        //     <View style={styles.card}>
-        //         <CustomCard
-        //             image={require("./src/assets/image.jpeg")}
-        //             title="One piece collection"
-        //             price={30}
-        //             city="Antibes 06600"
-        //         />
-        //     </View>
-        // </SafeAreaView>
-        <ListingDetailsScreen />
-    );
+    return <ViewImageScreen />;
 };
 
 const styles = StyleSheet.create({
     container: {
         height: "100%",
         backgroundColor: "#f4f4f4",
-    },
-    card: {
-        alignItems: "center",
     },
 });
 
