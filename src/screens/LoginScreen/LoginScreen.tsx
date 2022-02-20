0a49404b8e3ad57085edcafd51346437342cc8d1
Souleimane Kouygou <soulei.dev@gmail.com>
2022-02-20 19:51:01 +0100

    Formik installation, refactors LoginScreen

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,43 +1,53 @@
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 import { Image, StyleSheet } from "react-native";
 import CustomButton from "../../components/CustomButton/CustomButton";
 import CustomInput from "../../components/CustomInput/CustomInput";
 import CustomScreen from "../../components/CustomScreen/CustomScreen";
+import { Formik } from "formik";
+
 import colors from "../../config/colors";
 
 const LoginScreen: FC = () => {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
     return (
         <CustomScreen style={styles.container}>
             <Image
                 style={styles.logo}
                 source={require("../../assets/logo.png")}
             />
-            <CustomInput
-                textContentType="emailAddress"
-                keyboardType="email-address"
-                onChangeText={(text) => setEmail(text)}
-                autoCorrect={false}
-                autoCapitalize="none"
-                placeholder="Email"
-                icon="email"
-            />
-            <CustomInput
-                textContentType="password"
-                autoCorrect={false}
-                onChangeText={(text) => setPassword(text)}
-                autoCapitalize="none"
-                placeholder="Mot de passe"
-                secureTextEntry
-                icon="lock"
-            />
-            <CustomButton
-                label="Connexion"
-                onPress={() => console.log(email, password)}
-                buttonColor={colors.primary}
-                labelColor={colors.white}
-            />
+
+            <Formik
+                initialValues={{ email: "", password: "" }}
+                onSubmit={(values) => console.log(values)}
+            >
+                {({ handleChange, handleSubmit }) => (
+                    <>
+                        <CustomInput
+                            textContentType="emailAddress"
+                            keyboardType="email-address"
+                            onChangeText={handleChange("email")}
+                            autoCorrect={false}
+                            autoCapitalize="none"
+                            placeholder="Email"
+                            icon="email"
+                        />
+                        <CustomInput
+                            textContentType="password"
+                            autoCorrect={false}
+                            onChangeText={handleChange("password")}
+                            autoCapitalize="none"
+                            placeholder="Mot de passe"
+                            secureTextEntry
+                            icon="lock"
+                        />
+                        <CustomButton
+                            label="Connexion"
+                            onPress={handleSubmit}
+                            buttonColor={colors.primary}
+                            labelColor={colors.white}
+                        />
+                    </>
+                )}
+            </Formik>
         </CustomScreen>
     );
 };
