8e0c43a903d292af2eb91850981ed0e9068d2384
Souleimane Kouygou <soulei.dev@gmail.com>
2022-02-22 18:34:57 +0100

    Fix Swipeable issue with android - Refactor ListItem to show chevron icon

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
 import React, { FC, useState } from "react";
 import WelcomeScreen from "./src/screens/WelcomeScreen/WelcomeScreen";
 import CustomCard from "./src/components/CustomCard/CustomCard";
-import { SafeAreaView, StyleSheet, Switch, Text, View } from "react-native";
+import {
+    FlatList,
+    SafeAreaView,
+    StyleSheet,
+    Switch,
+    Text,
+    View,
+} from "react-native";
 import ListingDetailsScreen from "./src/screens/ListingDetailsScreen/ListingDetailsScreen";
 import ViewImageScreen from "./src/screens/ViewImageScreen/ViewImageScreen";
 import MessagesScreen from "./src/screens/MessagesScreen/MessagesScreen";
@@ -15,9 +22,37 @@ import CustomPicker from "./src/components/CustomPicker/CustomPicker";
 import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen/RegisterScreen";
 import ListingEditScreen from "./src/screens/ListingEditScreen/ListingEditScreen";
+import ListItemSeparator from "./src/components/ListItemSeparator/ListItemSeparator";
+import ListItemDeleteAction from "./src/components/ListItemDeleteAction/ListItemDeleteAction";
+
+const items = [
+    { title: "T1", subtitle: "D1", image: require("./src/assets/148327.jpg") },
+    { title: "T2", subtitle: "D2", image: require("./src/assets/219857.png") },
+];
 
 const App: FC = () => {
-    return <ListingEditScreen />;
+    return (
+        <CustomScreen>
+            <FlatList
+                data={items}
+                keyExtractor={(item) => item.title}
+                renderItem={({ item }) => (
+                    <ListItem
+                        showChevron
+                        title={item.title}
+                        subTitle={item.subtitle}
+                        image={item.image}
+                        renderRightActions={() => (
+                            <ListItemDeleteAction
+                                onPress={() => console.log(item)}
+                            />
+                        )}
+                    />
+                )}
+                ItemSeparatorComponent={ListItemSeparator}
+            />
+        </CustomScreen>
+    );
 };
 
 export default App;
--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -10,6 +10,8 @@ import {
 import colors from "../../config/colors";
 import CustomText from "../CustomText/CustomText";
 import Swipeable from "react-native-gesture-handler/Swipeable";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 type Props = {
     title: string;
@@ -21,6 +23,7 @@ type Props = {
         dragAnimatedValue: Animated.AnimatedInterpolation
     ) => React.ReactNode;
     IconComponent?: JSX.Element;
+    showChevron?: boolean;
 };
 
 const ListItem: FC<Props> = ({
@@ -30,32 +33,57 @@ const ListItem: FC<Props> = ({
     onPress,
     renderRightActions,
     IconComponent,
+    showChevron,
 }) => {
     return (
-        <Swipeable renderRightActions={renderRightActions}>
-            <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
-                <View style={styles.container}>
-                    {IconComponent}
-                    {image && <Image style={styles.image} source={image} />}
-                    <View style={styles.detailsContainer}>
-                        <CustomText style={styles.title}>{title}</CustomText>
-                        {subTitle && (
-                            <CustomText style={styles.subTitle}>
-                                {subTitle}
-                            </CustomText>
-                        )}
+        <GestureHandlerRootView>
+            <Swipeable renderRightActions={renderRightActions}>
+                <TouchableHighlight
+                    underlayColor={colors.light}
+                    onPress={onPress}
+                >
+                    <View style={styles.container}>
+                        <View style={styles.content}>
+                            {IconComponent}
+                            {image && (
+                                <Image style={styles.image} source={image} />
+                            )}
+                            <View style={styles.detailsContainer}>
+                                <CustomText style={styles.title}>
+                                    {title}
+                                </CustomText>
+                                {subTitle && (
+                                    <CustomText style={styles.subTitle}>
+                                        {subTitle}
+                                    </CustomText>
+                                )}
+                            </View>
+                        </View>
+                        {showChevron ? (
+                            <View style={styles.chevronIcon}>
+                                <MaterialCommunityIcons
+                                    name="chevron-right"
+                                    color={colors.gray}
+                                    size={25}
+                                />
+                            </View>
+                        ) : null}
                     </View>
-                </View>
-            </TouchableHighlight>
-        </Swipeable>
+                </TouchableHighlight>
+            </Swipeable>
+        </GestureHandlerRootView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
+        justifyContent: "space-between",
         flexDirection: "row",
         padding: 15,
         backgroundColor: colors.white,
+    },
+    content: {
+        flexDirection: "row",
     },
     image: {
         width: 70,
@@ -71,6 +99,9 @@ const styles = StyleSheet.create({
     detailsContainer: {
         justifyContent: "center",
         marginLeft: 10,
+    },
+    chevronIcon: {
+        justifyContent: "center",
     },
 });
 
