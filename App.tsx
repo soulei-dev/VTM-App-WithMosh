5cffacda5c34374f4587511b8c5192257371c4af
Souleimane Kouygou <soulei.dev@gmail.com>
2022-03-15 13:15:07 +0100

    WIP: Navigation

--- App.tsx
+++ App.tsx
@@ -1,37 +1,44 @@
 import React, { FC, useEffect, useState } from "react";
-import WelcomeScreen from "./src/screens/WelcomeScreen/WelcomeScreen";
-import CustomCard from "./src/components/CustomCard/CustomCard";
-import {
-    Button,
-    FlatList,
-    SafeAreaView,
-    StyleSheet,
-    Switch,
-    Text,
-    View,
-    Image,
-} from "react-native";
-import ListingDetailsScreen from "./src/screens/ListingDetailsScreen/ListingDetailsScreen";
-import ViewImageScreen from "./src/screens/ViewImageScreen/ViewImageScreen";
-import MessagesScreen from "./src/screens/MessagesScreen/MessagesScreen";
-import AccountScreen from "./src/screens/AccountScreen/AccountScreen";
-import CustomIcon from "./src/components/CustomIcon/CustomIcon";
+import { Text } from "react-native";
 import CustomScreen from "./src/components/CustomScreen/CustomScreen";
-import ListItem from "./src/components/ListItem/ListItem";
-import ListingsScreen from "./src/screens/ListingsScreen/ListingsScreen";
-import CustomInput from "./src/components/CustomInput/CustomInput";
-import CustomPicker from "./src/components/CustomPicker/CustomPicker";
-import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
-import RegisterScreen from "./src/screens/RegisterScreen/RegisterScreen";
-import ListingEditScreen from "./src/screens/ListingEditScreen/ListingEditScreen";
-import ListItemSeparator from "./src/components/ListItemSeparator/ListItemSeparator";
-import ListItemDeleteAction from "./src/components/ListItemDeleteAction/ListItemDeleteAction";
-import * as ImagePicker from "expo-image-picker";
-import CustomImageInput from "./src/components/CustomImageInput/CustomImageInput";
-import CustomImageInputList from "./src/components/CustomImageInputList/CustomImageInputList";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tweets = () => {
+  return (
+    <CustomScreen>
+      <Text>Tweets</Text>
+    </CustomScreen>
+  );
+};
+
+const TweetsDetails = () => {
+  return (
+    <CustomScreen>
+      <Text>Tweets Details</Text>
+    </CustomScreen>
+  );
+};
+
+const Stack = createStackNavigator();
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Tweets" component={Tweets}></Stack.Screen>
+      <Stack.Screen
+        name="TweetsDetails"
+        component={TweetsDetails}
+      ></Stack.Screen>
+    </Stack.Navigator>
+  );
+};
 
 const App: FC = () => {
-    return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 };
 
 export default App;
--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from "react";
 import {
-    View,
-    TextInput,
-    StyleSheet,
-    Platform,
-    TextInputProps,
+  View,
+  TextInput,
+  StyleSheet,
+  Platform,
+  TextInputProps,
 } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
@@ -12,45 +12,45 @@ import colors from "../../config/colors";
 import defaultStyles from "../../config/styles";
 
 export type CustomInputProps = {
-    icon?: React.ComponentProps<typeof MaterialCommunityIcons>["name"];
-    width?: string | number;
+  icon?: React.ComponentProps<typeof MaterialCommunityIcons>["name"];
+  width?: string | number;
 } & TextInputProps;
 
 const CustomInput: FC<CustomInputProps> = ({
-    icon,
-    width = "100%",
-    ...props
+  icon,
+  width = "100%",
+  ...props
 }) => {
-    return (
-        <View style={[styles.container, { width }]}>
-            {icon && (
-                <MaterialCommunityIcons
-                    name={icon}
-                    size={20}
-                    style={styles.icon}
-                    color={colors.medium}
-                />
-            )}
-            <TextInput
-                placeholderTextColor={colors.grayLight}
-                style={defaultStyles.text}
-                {...props}
-            />
-        </View>
-    );
+  return (
+    <View style={[styles.container, { width }]}>
+      {icon && (
+        <MaterialCommunityIcons
+          name={icon}
+          size={20}
+          style={styles.icon}
+          color={colors.medium}
+        />
+      )}
+      <TextInput
+        placeholderTextColor={colors.grayLight}
+        style={defaultStyles.text}
+        {...props}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        borderRadius: 25,
-        flexDirection: "row",
-        backgroundColor: "#F7F4F4",
-        padding: 15,
-        marginVertical: 10,
-    },
-    icon: {
-        marginRight: 10,
-    },
+  container: {
+    borderRadius: 25,
+    flexDirection: "row",
+    backgroundColor: "#F7F4F4",
+    padding: 15,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
 });
 
 export default CustomInput;
