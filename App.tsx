f2529d52d8c00f551fb36c29fa0b542a77d88e9e
Souleimane Kouygou <soulei.dev@gmail.com>
2022-03-16 17:10:46 +0100

    Navigating between screens

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
 import React, { FC, useEffect, useState } from "react";
-import { Text } from "react-native";
+import { Text, Button } from "react-native";
 import CustomScreen from "./src/components/CustomScreen/CustomScreen";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+
+const Link = () => {
+  const navigation = useNavigation();
+  return (
+    <Button
+      title="Click"
+      onPress={() => navigation.navigate("TweetsDetails")}
+    />
+  );
+};
 
 const Tweets = () => {
   return (
     <CustomScreen>
       <Text>Tweets</Text>
+      <Link />
     </CustomScreen>
   );
 };
@@ -24,11 +35,8 @@ const Stack = createStackNavigator();
 const StackNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Tweets" component={Tweets}></Stack.Screen>
-      <Stack.Screen
-        name="TweetsDetails"
-        component={TweetsDetails}
-      ></Stack.Screen>
+      <Stack.Screen name="Tweets" component={Tweets} />
+      <Stack.Screen name="TweetsDetails" component={TweetsDetails} />
     </Stack.Navigator>
   );
 };
