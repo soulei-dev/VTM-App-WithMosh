fc31b50ee02bd1c627b04978d6b8042521f33352
Souleimane Kouygou <soulei.dev@gmail.com>
2022-04-20 07:54:30 +0200

    Extending the API Layer

--- src/api/listings.js
+++ src/api/listings.js
@@ -3,7 +3,29 @@ import client from "./client";
 const endpoint = "/listings";
 
 const getListings = () => client.get(endpoint);
+const addListing = (listing) => {
+  const data = new FormData();
+
+  data.append("title", listing.title);
+  data.append("price", listing.price);
+  data.append("categoryId", listing.category.value);
+  data.append("description", listing.description);
+
+  listing.images.forEach((images, index) =>
+    data.append("images", {
+      name: "images" + index,
+      type: "images/jpeg",
+      uri: image,
+    })
+  );
+
+  if (listing.location)
+    data.append("location", JSON.stringify(listing.location));
+
+  return client.post(endpoint, data);
+};
 
 export default {
   getListings,
+  addListing,
 };
