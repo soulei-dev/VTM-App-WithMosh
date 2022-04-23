c77d08b3484565e070fd2197537829257dd80f76
Souleimane Kouygou <soulei.dev@gmail.com>
2022-04-23 16:47:40 +0200

    Fix request permission for location deprecated

--- src/hooks/useLocation.tsx
+++ src/hooks/useLocation.tsx
@@ -2,26 +2,26 @@ import { useState, useEffect } from "react";
 import * as Location from "expo-location";
 
 const useLocation = () => {
-    const [location, setLocation] = useState<any>();
+  const [location, setLocation] = useState<any>();
 
-    const getLocation = async () => {
-        try {
-            const { granted } = await Location.requestPermissionsAsync();
-            if (!granted) return;
-            const {
-                coords: { latitude, longitude },
-            }: any = await Location.getLastKnownPositionAsync();
-            setLocation({ latitude, longitude });
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  const getLocation = async () => {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (!granted) return;
+      const {
+        coords: { latitude, longitude },
+      }: any = await Location.getLastKnownPositionAsync();
+      setLocation({ latitude, longitude });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    useEffect(() => {
-        getLocation();
-    }, []);
+  useEffect(() => {
+    getLocation();
+  }, []);
 
-    return location;
+  return location;
 };
 
 export default useLocation;
