6e8d7005967e92fd6bdcf86e0222488fe1bad81a
Souleimane Kouygou <soulei.dev@gmail.com>
2022-04-25 11:02:36 +0200

    Implementing a Cache Layer

--- App.tsx
+++ App.tsx
@@ -2,13 +2,31 @@ import React, { FC } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthNavigator from "./src/navigation/AuthNavigator";
 import navigationTheme from "./src/navigation/navigationTheme";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+// const App: FC = () => {
+//   return (
+//     <NavigationContainer theme={navigationTheme}>
+//       <AuthNavigator />
+//     </NavigationContainer>
+//   );
+// };
+
+// export default App;
 const App: FC = () => {
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AuthNavigator />
-    </NavigationContainer>
-  );
+  const storeData = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+      const value = await AsyncStorage.getItem("person");
+      if (value !== null) {
+        JSON.parse(value);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  storeData();
+  return null;
 };
 
 export default App;
