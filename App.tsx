06094811f1265e5a0c5092072a0dc8b9f4b8cbc5
Souleimane Kouygou <soulei.dev@gmail.com>
2022-05-04 18:56:48 +0200

    Controlling the Splash Screen

--- App.tsx
+++ App.tsx
@@ -7,9 +7,11 @@ import AuthContext from "./src/auth/context";
 import AppNavigator from "./src/navigation/AppNavigator";
 import authStorage from "./src/auth/storage";
 import jwtDecode from "jwt-decode";
+import AppLoading from "expo-app-loading";
 
 const App: FC = () => {
   const [user, setUser] = useState<any>();
+  const [isReady, setIsReady] = useState<boolean>(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
@@ -17,9 +19,14 @@ const App: FC = () => {
     setUser(jwtDecode(token));
   };
 
-  useEffect(() => {
-    restoreToken();
-  }, []);
+  if (!isReady)
+    return (
+      <AppLoading
+        startAsync={restoreToken}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
